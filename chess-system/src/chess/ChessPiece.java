4af42553ee1000843b9a5777d9d258b3d639855f
VitorPirolo19 <vpirolo10@gmail.com>
2024-01-15 13:47:30 -0300

    feat: implementing possible moves of Rook

--- chess-system/src/chess/ChessPiece.java
+++ chess-system/src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
@@ -12,5 +13,10 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    protected boolean isThereOpponentPiece(Position position){
+        ChessPiece aux =(ChessPiece) getBoard().piece(position);
+        return aux != null && aux.getColor() != color;
     }
 }
