16f2e8a9e6efdcbd6cb089e9e30ede080f61cd1e
VitorPirolo19 <vpirolo10@gmail.com>
2024-01-17 22:01:48 -0300

    feat: Check logic

--- chess-system/src/chess/ChessPiece.java
+++ chess-system/src/chess/ChessPiece.java
@@ -15,6 +15,10 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public ChessPosition getChessPosition(){
+        return ChessPosition.fromPosition(position);
+    }
+
     protected boolean isThereOpponentPiece(Position position){
         ChessPiece aux =(ChessPiece) getBoard().piece(position);
         return aux != null && aux.getColor() != color;
